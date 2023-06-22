1bc2bd7dba89f18b78792514b22ddf1d9b8dc4d1
Yilver Molina Hurtatiz <yilver.molina@globant.com>
2023-06-22 15:18:11 -0500

    Added button test file

---
+++ cypress/component/button.cy.ts
@@ -0,0 +1,5 @@
+import { ButtonComponent } from 'src/app/shared/components/button/button.component'
+
+describe('button component', () => {
+    
+});
--- cypress/component/character.cy.ts
+++ cypress/component/character.cy.ts
@@ -1,8 +1,8 @@
 import { CharacterComponent } from 'src/app/characters/character/character.component'
 import { mockCharacter } from 'src/test/characters/mock-characters'
 
-describe('character.cy.ts', () => {
-  it('character', () => {
+describe('character component', () => {
+  it('should display the character details', () => {
     cy.mount(CharacterComponent, {
       componentProperties: {
         character: mockCharacter
